2770fa53fd7f590a0915681749370b12accf4b65
hectorponsoy <hectorjosue65@gmail.com>
2023-05-03 20:13:40 -0600

    logic to encrypt text is done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,42 @@
-function txt_area_pressed() {
-    document.getElementById("boy_img").style.display = "none";
-    document.getElementById("no_message_found_img").style.display = "none";
+class Encrypt {
+    constructor(text_from_txt, encrypted_text) {
+        this.text_from_txt = text_from_txt;
+        this.encrypted_text = encrypted_text;
+    }
+
+    getTextFromtxt() {
+        this.text = '';
+        this.text = document.getElementById('input_for_enter_text').value;
+        return this.text;
+    }
+
+    encryptText() {
+        let text_to_encrypt = this.getTextFromtxt();
+
+        document.getElementById("boy_img").style.display = "none";
+        document.getElementById("no_message_found_img").style.display = "none";
+
+        text_to_encrypt = text_to_encrypt.replaceAll('a', 'ai');
+        text_to_encrypt = text_to_encrypt.replaceAll('e', 'enter');
+        text_to_encrypt = text_to_encrypt.replaceAll('i', 'imes');
+        text_to_encrypt = text_to_encrypt.replaceAll('o', 'ober');
+        text_to_encrypt = text_to_encrypt.replaceAll('u', 'ufat');
+
+        this.encrypted_text = document.getElementById('text_area_result');
+        this.encrypted_text.value = text_to_encrypt;
+
+        return text_to_encrypt;
+    }
 }
+
+function btn_blue_pressed() {
+    console.log('Btn blue is pressed');
+    let objectEncrypt = new Encrypt();
+    objectEncrypt.encryptText();
+}
+
+
+
+
+
+
