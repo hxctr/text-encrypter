cc1d05a4466e83ffb85d2fd6abdfb6c736c0dd23
hectorponsoy <hectorjosue65@gmail.com>
2023-05-03 21:33:53 -0600

    corrected logic to encrypt and finished decrypt

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ class Encrypt {
     getTextFromtxt() {
         this.text = '';
         this.text = document.getElementById('input_for_enter_text').value;
+
         return this.text;
     }
 
@@ -16,16 +17,61 @@ class Encrypt {
         document.getElementById("boy_img").style.display = "none";
         document.getElementById("no_message_found_img").style.display = "none";
 
-        text_to_encrypt = text_to_encrypt.replaceAll('a', 'ai');
-        text_to_encrypt = text_to_encrypt.replaceAll('e', 'enter');
-        text_to_encrypt = text_to_encrypt.replaceAll('i', 'imes');
-        text_to_encrypt = text_to_encrypt.replaceAll('o', 'ober');
-        text_to_encrypt = text_to_encrypt.replaceAll('u', 'ufat');
+        text_to_encrypt = text_to_encrypt.split('');
+
+        for (let i = 0; i < text_to_encrypt.length; i++) {
+            if (text_to_encrypt[i] == 'a') {
+                text_to_encrypt[i] = 'ai'
+            }else if (text_to_encrypt[i] == 'e') {
+                text_to_encrypt[i] = 'enter'
+            }else if (text_to_encrypt[i] == 'i') {
+                text_to_encrypt[i] == 'imes';
+            }else if (text_to_encrypt[i] == 'o') {
+                text_to_encrypt[i] = 'ober'
+            }else if (text_to_encrypt[i] == 'u') {
+                text_to_encrypt[i] = 'ufat';
+            }
+            
+        }
+
+        let newText = '';
+        for (let j = 0; j < text_to_encrypt.length; j++) {
+            newText += text_to_encrypt[j];
+            
+        }
 
         this.encrypted_text = document.getElementById('text_area_result');
-        this.encrypted_text.value = text_to_encrypt;
+        this.encrypted_text.value = newText;
+    }
+}
 
-        return text_to_encrypt;
+class Decrypt {
+    constructor(text_from_txt, decrypted_text) {
+        this.text_from_txt = text_from_txt;
+        this.decrypted_text = decrypted_text;
+    }
+
+    getTextFromtxt() {
+        this.text = '';
+        this.text = document.getElementById('input_for_enter_text').value;
+
+        return this.text;
+    }
+
+    decryptText() {
+        let text_to_decrypt = this.getTextFromtxt();
+
+        document.getElementById("boy_img").style.display = "none";
+        document.getElementById("no_message_found_img").style.display = "none";
+
+        text_to_decrypt = text_to_decrypt.replaceAll('ai', 'a');
+        text_to_decrypt = text_to_decrypt.replaceAll('enter', 'e');
+        text_to_decrypt = text_to_decrypt.replaceAll('imes', 'i');
+        text_to_decrypt = text_to_decrypt.replaceAll('ober', 'o');
+        text_to_decrypt = text_to_decrypt.replaceAll('ufat', 'u');
+
+        this.decrypted_text = document.getElementById('text_area_result');
+        this.decrypted_text.value = text_to_decrypt;
     }
 }
 
@@ -35,8 +81,10 @@ function btn_blue_pressed() {
     objectEncrypt.encryptText();
 }
 
-
-
-
+function btn_white_pressed() {
+    console.log('Btn white is pressed');
+    let objectDecrypt = new Decrypt();
+    objectDecrypt.decryptText();
+}
 
 
